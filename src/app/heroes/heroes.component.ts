000ce34bc95bc7b9384a02ce195787b32f55e8c1
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-24 15:10:17 -0500

    looping through heros

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock.heros';
 import { UpperCasePipe } from '@angular/common';
 import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-
+  heroes=HEROES
+  
   hero:Hero={
     id:1,
     name: "Mace Window"
