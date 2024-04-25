23599e6ae2de43ae72632f5af01a1fe30a839730
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-25 14:18:35 -0500

    adding services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock.heros';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 
 
@@ -9,13 +10,21 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 
+
+
+
+
 export class HeroService {
   getHeroes(): Observable <Hero[]> {
     const heros = of(HEROES);
+    this.messageService.add('HeroS:Fetched heros')
     return heros
   }
 
+  constructor(private messageService: MessageService) { }
 
 
-  constructor() { }
+
+
+
 }
